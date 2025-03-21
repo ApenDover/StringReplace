d7d41019f27d3b63e9dc164050053a06c8260ef7
Andrey Tsaregorodtsev <a.tsaregorodtsev@xfer.world>
2025-03-21 10:00:08 +0300

    task

--- src/main/java/ts/andrey/Main.java
+++ src/main/java/ts/andrey/Main.java
@@ -1,7 +1,15 @@
 package ts.andrey;
 
 public class Main {
+
+    /**
+     * Нужно написать метод, который на вход принимает список строк
+     * возвращает список строк в котором:
+     * - все строки upperCase
+     * - все строки которые начинаются на буквы A и B должны быть удалены
+     * */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
     }
+
 }
